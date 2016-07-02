de1aa8ddfd0c266bb7252c795039e71166968772
Gustavo Ehrhardt <gus.ehr@gmail.com>
2016-07-02 15:51:38 -0300

    firebase database and auth config

---
+++ firebaseImport.js
@@ -0,0 +1,19 @@
+<script src="https://www.gstatic.com/firebasejs/3.1.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyApoVyKhL09TVdRSHcxPGqX9tDJfOKbcfE",
+    authDomain: "inmudanca.firebaseapp.com",
+    databaseURL: "https://inmudanca.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
+</script>
+
+<!-- Auth
+ID do cliente da Web
+498877740939-gpi27kj1j1seh9efmlntu8o5dv4ggo3t.apps.googleusercontent.com
+Chave secreta do cliente da Web
+Bf1G5YjlglHG1VJQOYnFN6a7
+--//>
+
