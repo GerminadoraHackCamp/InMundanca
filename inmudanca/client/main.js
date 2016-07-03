5c379ad9be1c6b4f381fa8ee23584cb7d8b5904e
Luiz Sales <quick.br@gmail.com>
2016-07-03 16:37:25 -0300

    Update

--- inmudanca/client/main.js
+++ inmudanca/client/main.js
@@ -1,22 +1,14 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+Meteor.subscribe('dadosImoveis');
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+console.log(Imoveis.find().count());
+
+Template.main.helpers({
+  imoveis() {
+    return Imoveis.find();
   },
 });
---
+++ inmudanca/lib/mongo.js
@@ -0,0 +1,3 @@
+import { Meteor } from 'meteor/meteor';
+
+Imoveis = new Mongo.Collection("imovel");
